7f490c0b4b025bfe7995e139cea10601a7b5a310
abdellahchatioui <abdellahchatioui123@gmail.com>
2026-03-19 21:21:59 +0000

    feat: implement Auth Service

--- src/main/java/com/example/habittracker/service/AuthService.java
+++ src/main/java/com/example/habittracker/service/AuthService.java
@@ -1,4 +1,49 @@
 package com.example.habittracker.service;
 
+import com.example.habittracker.Entity.User;
+import com.example.habittracker.dto.LoginRequest;
+import com.example.habittracker.repository.UserRepository;
+import com.example.habittracker.security.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class AuthService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    public String login(LoginRequest request) {
+        User user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+            throw new RuntimeException("Invalid credentials");
+        }
+
+        return jwtUtil.generateToken(user.getEmail());
+    }
+
+    public void register(User newUser) {
+        Optional<User> existingUser = userRepository.findByEmail(newUser.getEmail());
+
+        if (existingUser.isPresent()) {
+            throw new RuntimeException("Email already exists");
+        }
+
+        newUser.setPassword(passwordEncoder.encode(newUser.getPassword()));
+        newUser.setRole("USER");
+        newUser.setEnabled(true);
+
+        userRepository.save(newUser);
+    }
 }
